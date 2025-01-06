53733c1e8130f764ab807cd9793cdeefce3f189b
Ryan King <161281690+Ryan-King-Dev@users.noreply.github.com>
2025-01-05 18:57:44 -0800

    utility bar completed

---
+++ components/utility.php
@@ -0,0 +1,23 @@
+<div id="utility">
+
+    <a id="logoLink" href="/default.php" ?>
+        <img id="logo" src="../images/cealogo.png" alt="CEA logo">
+
+    </a>
+
+    <form id="formSearch">
+        <input id="search" type="text" placeholder="Search...">
+    </form>
+
+    <form id="login">
+        <input id="username" type="text" placeholder="Username">
+        <input id="password" type="text" placeholder="Password">
+        <div id="logStatus">
+            <input id="stayLoggedIn" name="stayLoggedIn" type="checkbox">
+            <label for="stayLoggedIn">Keep me logged in</label>
+            <!-- <input id="loginSubmit" type="submit" value="Log In"> -->
+            <button id="loginSubmit" type="submit" onclick="alert('Logged In')">Log In</button>
+        </div>
+    </form>
+
+</div>
